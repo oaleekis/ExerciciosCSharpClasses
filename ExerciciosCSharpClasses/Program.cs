0e5acada662cd7627d444505430529b101ba0def
Alex <oalexsanderm@gmail.com>
2023-05-19 11:22:22 -0300

    Conclusão Ex Classes

--- ExerciciosCSharpClasses/Program.cs
+++ ExerciciosCSharpClasses/Program.cs
@@ -146,6 +146,28 @@ namespace ExerciciosCSharpClasses
             }
             */
             #endregion
+
+            // Exercício de fixação membros estáticos
+
+            #region Ex - 01
+            //Faça um programa para ler a cotação do dólar, e depois um valor em dólares a ser comprado por
+            //uma pessoa em reais. Informar quantos reais a pessoa vai pagar pelos dólares, considerando ainda
+            //que a pessoa terá que pagar 6 % de IOF sobre o valor em dólar. Criar uma classe ConversorDeMoeda
+            //para ser responsável pelos cálculos.
+
+            /*
+            Console.WriteLine("Qual é a cotação do dólar: ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Quantos dólares você vai comprar: ");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double resultado = ConversorDeMoeda.DolarParaReal(quantia, cotacao);
+            Console.WriteLine($"Valor a ser pago em reais = {resultado.ToString("F2", CultureInfo.InvariantCulture)}");
+            */
+            #endregion
+
+
         }
     }
 }
